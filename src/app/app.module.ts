2ec0fb344ac331615e6db0dea4d981cddfb1155b
Joao Allisson da Costa Silva <Joao.AllissondaCostaSilva@thomsonreuters.com>
2022-12-27 22:29:32 -0300

    initial structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { DetailsComponent } from './pages/details/details.component';
+import { HeaderComponent } from './template-components/header/header.component';
+import { FooterComponent } from './template-components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DetailsComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
